b55f3f2143a9f88046cfd80222fb575c6aad138b
MateLynx <62848024+MateLynx@users.noreply.github.com>
2020-03-30 15:54:29 +0200

    Batch update

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+/*
+ * active menu widget for Silex
+ * create an element which links to an anchor, e.g. an element with a link to #anchor1
+ * add the css class "anchor-link" to this element
+ * create an element which is the anchor, e.g. an element with the css class "anchor1"
+ * when the user clicks on the link, the scroll slides until the element is visible
+ * when the user slides and the element is visible, the link gets a css class "active-menu"
+ */
+$(function() {
+    var container = $('.anchor-container');
+    var containerTop = 0;
+    $(window).scroll(checkScroll);
+    checkScroll();
+    function checkScroll(){
+        var fromTop = $(this).scrollTop();
+        if(containerTop < fromTop) {
+           container.addClass('offscreen');
+        }
+        else {
+           container.removeClass('offscreen');
+        }
+    }
+});
